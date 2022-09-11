8a398c1008da649c301b7c0f6a2bfa38eadd3e25
Your Name <josevictormobile@gmail.com>
2022-09-11 20:04:32 -0300

    exercicio 1095

---
+++ beecrowd1095.cs
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+     int i=1, j=60;
+            while (j >=0)
+            {
+                Console.WriteLine($"I={i} J={j}");
+                i += 3;
+                j -= 5;
+            }
+ 
+    return 0;
+}
