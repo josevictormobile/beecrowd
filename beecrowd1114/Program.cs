87ccb191fe10f86f5addb9dd0a603fc637e50c7a
Your Name <josevictormobile@gmail.com>
2022-09-26 20:55:56 -0300

    resolvido com recursão

---
+++ beecrowd1114/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bee1114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(FixedPassword());
+        }
+
+        static string FixedPassword(){
+            string pass = Console.ReadLine();
+            if(pass!="2002"){
+                return "Senha Invalida\n"+FixedPassword();
+            }else{
+                return "Acesso Permitido";
+            }
+        }
+    }
+}
