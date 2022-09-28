05682da86e9f2d2f211b9858cf52e9c120039593
Your Name <josevictormobile@gmail.com>
2022-09-27 22:41:50 -0300

    outro exercício

---
+++ beecrowd1115/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        var resposta = Quadrant();
+        Console.WriteLine(resposta.Remove(resposta.Length - 1));
+    }
+
+    static string Quadrant(){
+        var linha = Console.ReadLine();
+        var x = int.Parse(linha.Split(" ")[0]);
+        var y = int.Parse(linha.Split(" ")[1]);
+        if(x==0||y==0){
+            return "";
+        }
+        if(x>0 && y>0){
+            return "primeiro\n"+Quadrant();
+        }
+        if(x>0 && y<0){
+            return "quarto\n"+Quadrant();
+        }
+        if(x<0 && y<0){
+            return "terceiro\n"+Quadrant();
+        }
+        if(x<0 && y>0){
+            return "segundo\n"+Quadrant();
+        }
+        return "";
+    }
+}
