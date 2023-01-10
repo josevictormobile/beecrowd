7d53e193032e3b8a3124a53bec4b53d8432e2ed9
Your Name <josevictormobile@gmail.com>
2023-01-09 23:52:26 -0300

    exercicio 1118 beecrowd

--- beecrowd1117/Program.cs
+++ beecrowd1117/Program.cs
@@ -7,32 +7,30 @@ namespace beecrowd1117
     {
         static void Main(string[] args)
         {
-           float number1=0f, number2=0f;
-           int count=0;
-           float score = float.Parse(Console.ReadLine());
-           if(ValidateScore(score)){
-                if(count==1){
-                    number2=score;
-                    Console.WriteLine("media = {0}", 
-                    AverageBetweenTwoNumbers(number1,number2).ToString("N2"));
-                }else{
-                    count++;
-                    number1=score;
-                }   
-           }
-           else{
-                Console.WriteLine("nota invalida");
-           }
-           
+            Console.WriteLine(ProcessScore(null));
         }
-
-        static bool ValidateScore(float score){
-            if(score>10 || score<0)
+        static bool ValidateScore(float score)
+        {
+            if (score > 10 || score < 0)
                 return false;
             return true;
         }
-        static float AverageBetweenTwoNumbers(float number1, float number2){
-            return (number1+number2)/2;
-        }    
+        
+        static string ProcessScore(float? firstValidScore)
+        {
+            var americanCulture = new CultureInfo("en-US");
+            float score = float.Parse(Console.ReadLine(), americanCulture);
+            if (ValidateScore(score))
+            {
+                if (firstValidScore == null)
+                {
+                    return ProcessScore(score);
+                }
+                var media =  ((firstValidScore + score) / 2);
+                var mediaFormatada = media.ToString().Replace(",",".");
+                return $"media = {mediaFormatada}";
+            }
+            return "nota invalida\n"+ProcessScore(firstValidScore);
+        }  
     }
 }
---
+++ beecrowd1118/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd1118
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputResult = ProcessScore(null) + ProgramLoop();
+            Console.WriteLine(outputResult);
+        }
+        static bool ValidateScore(float score)
+        {
+            if (score > 10 || score < 0)
+                return false;
+            return true;
+        }
+        static bool ValidateOption(int option)
+        {
+            if (option > 2 || option < 1)
+                return false;
+            return true;
+        }
+        static string ProgramLoop()
+        {
+            int option = int.Parse(Console.ReadLine());
+            
+            if (ValidateOption(option))
+            {
+                if(option==1)
+                    return "novo calculo (1-sim 2-nao)\n" + ProcessScore(null) + ProgramLoop();
+                return "novo calculo (1-sim 2-nao)";
+            }
+            else
+            {
+                return "novo calculo (1-sim 2-nao)\n"+ ProgramLoop();
+            }
+        }
+
+        static string ProcessScore(float? firstValidScore)
+        {
+            var americanCulture = new CultureInfo("en-US");
+            float score = float.Parse(Console.ReadLine(), americanCulture);
+            if (ValidateScore(score))
+            {
+                if (firstValidScore == null)
+                {
+                    return ProcessScore(score);
+                }
+                float media =  (((float)firstValidScore + score) / 2.0f);
+                var mediaFormatada = media.ToString("N2").Replace(",",".");
+                return $"media = {mediaFormatada}\n";
+            }
+            return "nota invalida\n"+ProcessScore(firstValidScore);
+        }
+    }
+}
