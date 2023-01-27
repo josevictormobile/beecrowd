dc7fa63d51a1e2743fbd01a8c5753ba87b7b8002
josevictormobile <josevictormobile@gmail.com>
2023-01-26 22:59:28 -0300

    exercicio bee1144

---
+++ bee1144/bee1144.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace bee1144._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=lines;i++){
+    Console.WriteLine($"{i} {i*i} {i*i*i}");
+        Console.WriteLine($"{i} {i*i+1} {i*i*i+1}");
+    
+        
+    
+        
+}//User josevictor who made this
+        }
+    }
+}
