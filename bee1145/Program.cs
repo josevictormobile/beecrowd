1ea379b14b289b1c2bf68a75ea89a7d981328089
victor.figueiredo <victor.figueiredo@aec.com.br>
2023-01-27 21:38:16 -0300

    confirmar

---
+++ bee1145/Program.cs
@@ -0,0 +1,14 @@
+﻿string input = Console.ReadLine();
+string[] input_array = input.Split(' ');
+int x = Convert.ToInt32(input_array[0]);
+int y = Convert.ToInt32(input_array[1]);
+
+for(int i=1;i<=y;i++){
+    
+    if(i%x==0){
+        Console.Write(i + "\n");
+    }else{
+        Console.Write(i + " ");
+    }
+        
+}
