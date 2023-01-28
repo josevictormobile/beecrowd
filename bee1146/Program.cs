e835f35e774a7c1235c6ff53f2ecde7a3960c3a7
victor.figueiredo <victor.figueiredo@aec.com.br>
2023-01-27 21:34:36 -0300

    error: Time limit exceeded

---
+++ bee1146/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string answear = RecursiveSolution();
+Console.WriteLine(answear.Remove(answear.Length-1));
+string RecursiveSolution(){
+    string result = string.Empty;
+    int x = Convert.ToInt32(Console.ReadLine());
+    if(x==0){
+        return string.Empty;
+    }
+    for(int i=1;i<=x;i++){
+        if(i==x){
+            result += i.ToString()+"\n";
+        }else{
+            result += i.ToString()+" ";
+        }
+    }
+    return result + RecursiveSolution();
+}
