25717fc25ffb50c836c3a97758b455045cce8303
victor.figueiredo <victor.figueiredo@aec.com.br>
2023-01-30 17:05:30 -0300

    exercicio 1150

---
+++ bee1150/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = Convert.ToInt32(Console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int answear = 0;
+while(z<=x){
+    z = Convert.ToInt32(Console.ReadLine());
+}
+for(int i=0;sum<=z;i++){
+    sum += x+i;
+    answear = i;
+}
+answear+=1;
+Console.WriteLine(answear);
