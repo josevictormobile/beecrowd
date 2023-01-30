8d2a1baa72ff87c37eebb6a28c1357069ad31c7b
victor.figueiredo <victor.figueiredo@aec.com.br>
2023-01-30 14:59:39 -0300

    exercicio 1149 resolvido

---
+++ bee1149/Program.cs
@@ -0,0 +1,16 @@
+﻿string input = Console.ReadLine();
+string[] inputs = input.Split(' ');
+int a = Convert.ToInt32(inputs[0]);
+int n = 0;
+int sum = 0;
+for(int i=1;i<inputs.Length;i++){
+    if(Convert.ToInt32(inputs[i])>0){
+        n = Convert.ToInt32(inputs[i]);
+        break;
+    }
+}
+
+for(int i=0;i<n;i++){
+    sum +=a+i;
+}
+System.Console.WriteLine(sum);
