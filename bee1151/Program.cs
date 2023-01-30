6ae42645bd81b7ed9e69ab958f5698bff95e0a7a
victor.figueiredo <victor.figueiredo@aec.com.br>
2023-01-30 17:41:07 -0300

    exercicio 1151

---
+++ bee1151/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+Console.WriteLine(Fibonacci());
+
+string Fibonacci()
+{
+    int n = Convert.ToInt32(Console.ReadLine());
+    string result = string.Empty;
+    if (n == 0)
+        return result;
+    if (n == 1)
+        return "0";
+    var fibonacci = new List<int> { 0, 1 };
+    int next;
+    for (int i = 1; i < n - 1; i++)
+    {
+        next = fibonacci[i] + fibonacci[i - 1];
+        fibonacci.Add(next);
+    }
+
+    foreach (var number in fibonacci)
+    {
+        result += number + " ";
+    }
+    return result.Remove(result.Length-1);
+}
