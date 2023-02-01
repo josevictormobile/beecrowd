a763133d5fe215d1f5ee9575e2372c8a20504690
victor.figueiredo <victor.figueiredo@aec.com.br>
2023-02-01 16:41:55 -0300

    exercicio 1154

--- bee1146/solution_python.py
+++ bee1146/solution_python.py
@@ -13,3 +13,15 @@ while True:
     result += str(x)
         
 print(result) 
+
+
+def RecursiveSolution(sum, amount):
+    age = int(input())
+    if age < 0:
+        return sum/amount
+    sum += age
+    amount+=1
+    return RecursiveSolution(sum,amount)
+print(str(RecursiveSolution(0,0))); 
+
+
---
+++ bee1154/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine(RecursiveSolution(0,0).ToString("N2")); 
+double RecursiveSolution(double sum, int amount){
+    int age = Convert.ToInt32(Console.ReadLine());
+    if(age<0){
+        return sum/amount;
+    }
+    sum += age;
+    amount++;
+    return RecursiveSolution(sum,amount);
+}
