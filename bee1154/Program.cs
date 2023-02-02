e61bb719fa61d0f7be757bed43442e515d52817b
josevictormobile <josevictormobile@gmail.com>
2023-02-01 23:36:31 -0300

    assinando autoria devido ter feito push em outra conta

--- bee1154/Program.cs
+++ bee1154/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine(RecursiveSolution(0,0).ToString("N2")); 
+﻿//assinando autoria pois novamente fiz push desse conteudo com outra conta
+Console.WriteLine(RecursiveSolution(0,0).ToString("N2")); 
 double RecursiveSolution(double sum, int amount){
     int age = Convert.ToInt32(Console.ReadLine());
     if(age<0){
