ba2cbe7cecc9a6a0311c43411ad913d44cdf3a28
victor.figueiredo <victor.figueiredo@aec.com.br>
2023-02-03 19:31:31 -0300

    exercicios 1156 e 1157

---
+++ bee1156/Program.cs
@@ -0,0 +1,8 @@
+﻿double denominator = 1;
+double s = 0;
+for(double numerator=1;numerator<=39;numerator+=2){
+    s += numerator/denominator;
+    denominator*=2;
+    
+}
+System.Console.WriteLine(s.ToString("N2"));
---
+++ bee1157/Program.cs
@@ -0,0 +1,5 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i<=n;i++){
+    if(n%i==0)
+        Console.WriteLine(i);
+}
