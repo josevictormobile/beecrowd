b4cec43f0f22ff18e61027311aebcd599d8383b6
Your Name <josevictormobile@gmail.com>
2023-02-10 21:17:37 -0300

    exercicio bee1159

---
+++ bee1159/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace bee1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resultado = RecursiveSolution();
+            Console.WriteLine(resultado.Remove(resultado.Length-1));
+        }
+
+        static string RecursiveSolution(){
+            int x = Convert.ToInt32(Console.ReadLine());
+            if(x==0) return string.Empty;
+            int sum=0;
+            int amount_even=0;
+            while(amount_even<5){
+                if(x%2==0){
+                    sum+=x;
+                    amount_even++;
+                }
+                x++;
+            }
+            return sum.ToString() + "\n" + RecursiveSolution();
+        }
+    }
+}
