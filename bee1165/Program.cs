985e20379197e7f94119d8dbace710bb4149aaf8
Your Name <josevictormobile@gmail.com>
2023-02-15 20:46:30 -0300

    iniciando exercicio 1165

---
+++ bee1165/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bee1165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
