830482aedcad46f5b4fe7a494675baa54c073d12
Your Name <josevictormobile@gmail.com>
2023-03-20 20:58:26 -0300

    exercicio bee1172

---
+++ bee1172/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bee1172
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayInt = new int[10];
+            for(int i=0;i<10;i++){
+                arrayInt[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i=0;i<10;i++){
+                if(arrayInt[i]<=0){
+                    arrayInt[i] = 1;
+                }
+                Console.WriteLine($"X[{i}] = {arrayInt[i]}");
+            }
+        }
+    }
+}
